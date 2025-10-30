8541e98965511aa107d754eaaa9ff9850071f482
noname0718 <thckdals3@gmail.com>
2025-10-30 13:29:04 +0900

    🎉프로젝트 시작

---
+++ src/test/kotlin/kr/ac/kumoh/s20220565/w25w08_controller/W25w08ControllerApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20220565.w25w08_controller
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W25w08ControllerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
